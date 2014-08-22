310bf939467636a57dc680fc995c1bdf8aa53db8
Matthieu Bué <matthieu.bue@gmail.com>
2014-08-22 12:16:07 +0200

    First commit
    Initial commit 8/22/14 12:15

---
+++ js/google-analytics.js
@@ -0,0 +1,10 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+var ga_account = document.getElementById("google-analytics-js").getAttribute("data-ga-account");
+var ga_domain = document.getElementById("google-analytics-js").getAttribute("data-ga-domain");
+
+ga('create',ga_account,ga_domain);
+ga('send','pageview');
---
+++ php/send-form.php
@@ -0,0 +1,43 @@
+<?php
+
+$name     = trim(stripslashes($_POST['i1']));
+$email    = trim(stripslashes($_POST['i2']));
+$botkick  = trim(stripslashes($_POST['i3']));
+$message  = nl2br(htmlentities(stripslashes(trim($_POST['t'])),ENT_QUOTES));
+$remip    = $_SERVER['REMOTE_ADDR'];
+
+if(!preg_match("/^[a-zA-ZàáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ\s]+$/i",$name)){
+	echo 'error_name';
+}else{
+	if(!preg_match("/^([0-9a-zA-Z]+[-._+&])*[0-9a-zA-Z]+@([-0-9a-zA-Z]+[.])+[a-zA-Z]{2,6}$/i",$email)){
+		echo 'error_email'; 
+	}else{
+		if($botkick != ""){
+			echo 'error_bot';
+		}else{
+			if($message == ""){
+				echo 'error_msg';
+			}else{
+
+				$body     = "<div style='max-width:600px'>\r\n";
+				$body    .= "Message de ".$name." &lt;".$email."&gt; [".$remip."]<br/><br/>\r\n";
+				$body    .= "<div style='background:#f0f0f0;border:1px solid #e0e0e0;padding:10px;color:#000;'>".$message."</div>\r\n";
+				$body    .= "</div>\r\n";
+
+				// en-tête du mail
+				$headers  = 'MIME-Version: 1.0' . "\r\n";
+				$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+				$headers .= 'From:'.$name.' '.'<'.$email.'>' . "\r\n";
+
+				// envoi du mail
+				if(mail("hello@twikito.com","[twikito.com] ".$name,$body,$headers)){
+					echo 'ok';
+				}else{
+					echo 'error_send';
+				}
+			}
+		}
+	}
+}
+
+?>
